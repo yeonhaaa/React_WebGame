f32692b757efb14659095b3952832cf9582a59b7
yeonhaaa <qkdxo101@naver.com>
2022-02-16 23:47:33 +0900

    webpack설치

--- numberbaseball/client.jsx
+++ numberbaseball/client.jsx
@@ -1,5 +1,9 @@
-const React = require('react');
-const ReactDom = require('react-dom');
-const NumberBaseball = require('./NumberBaseball');
+// const React = require('react'); // 노드의 모듈시스템
+import React from 'react';
+import ReactDom from 'react-dom';
+import { hot } from 'react-hot-loader/root';
+import NumberBaseball from './NumberBaseball';
+
+const Hot = hot(NumberBaseball);
 
 ReactDom.render(<NumberBaseball />, document.querySelector('#root'));
